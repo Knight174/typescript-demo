b7784c154047f0db116dc80e78810643e23456db
Knight174 <cpc4253@163.com>
2022-02-12 21:44:18 +0800

    添加组合类型

--- src/object-type.ts
+++ src/object-type.ts
@@ -20,7 +20,7 @@ const func3 = (x: number): number => x;
 // object
 const obj: object = {};
 
-// 函数参数如果是对象类型
+// 函数参数如果是对象类型（?号表示可选属性）
 function getStudentName (student: {name: string, age: number, level?: number}) {
   return student.name;
 }
@@ -29,3 +29,33 @@ const studentName = getStudentName({
   age: 18
 })
 console.log(studentName);
+
+// Union Types: 组合类型，注意分类处理不同类型的参数，否则报错！
+function func4 (id: number | string) {
+  console.log(id);
+}
+
+function printId(id: number | string) {
+  if (typeof id === "string") {
+    // In this branch, id is of type 'string'
+    console.log(id.toUpperCase());
+  } else {
+    // Here, id is of type 'number'
+    console.log(id);
+  }
+}
+
+function welcomePeople(x: string[] | string) {
+  if (Array.isArray(x)) {
+    // Here: 'x' is 'string[]'
+    console.log("Hello, " + x.join(" and "));
+  } else {
+    // Here: 'x' is 'string'
+    console.log("Welcome lone traveler " + x);
+  }
+}
+
+// Return type is inferred as number[] | string
+function getFirstThree(x: number[] | string) {
+  return x.slice(0, 3);
+}
